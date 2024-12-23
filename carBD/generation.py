999bdf0093f893baad5fe0bf2a03930b5c750c94
agapo <agapova.nelli@gmail.com>
2024-12-23 19:52:06 +0300

    update

---
+++ carBD/generation.py
@@ -0,0 +1,300 @@
+import random
+import string
+from __main__ import (BrandModel, ModelModel, CarModel, StatusModel, MethodModel, ClientModel, TariffModel,
+                      ViolationModel, RentModel, PaymentModel, RentViolationModel)
+from datetime import datetime, timedelta
+
+# БРЕНДЫ
+
+brand_names = [
+    "Audi", "BMW", "Chevrolet", "Dodge", "Ford",
+    "Honda", "Hyundai", "Jeep", "Kia", "Lexus",
+    "Mazda", "Mercedes-Benz", "Nissan", "Porsche",
+    "Subaru", "Tesla", "Toyota", "Volkswagen", "Volvo",
+    "Jaguar"
+]
+
+countries = [
+    "United States", "Germany", "Japan", "South Korea",
+    "Italy", "France", "United Kingdom", "China",
+    "India", "Sweden", "Spain", "Canada",
+    "Brazil", "Mexico", "Russia", "Czech Republic",
+    "Turkey", "Thailand", "Malaysia", "Australia"
+]
+
+
+def generate_brands(session):
+    for _ in range(10):
+        brand = BrandModel(
+            brand_name=random.choice(brand_names),
+            country=random.choice(countries)
+        )
+        session.add(brand)
+    session.commit()
+
+
+# МОДЕЛИ
+
+model_names = [
+    "Mustang", "Corvette", "Camry", "Civic", "3 Series", "C-Class",
+    "A4", "Altima", "Sonata", "Optima", "Impreza", "Mazda3",
+    "Golf", "911", "Model S", "ES", "Wrangler", "Charger", "S60", "F-Type"
+]
+
+
+def generate_models(session):
+    brand_ids = [brand.id for brand in session.query(BrandModel).all()]
+
+    for _ in range(10):
+        model_name = random.choice(model_names)
+        brand_id = random.choice(brand_ids) if brand_ids else None
+
+        new_model = ModelModel(model_name=model_name, brand_id=brand_id)
+        session.add(new_model)
+
+    session.commit()
+
+
+# МАШИНЫ
+
+def generate_vin():
+    characters = string.ascii_uppercase + string.digits
+    return ''.join(random.choices(characters, k=17))
+
+
+def generate_license_plate():
+    letters = string.ascii_uppercase
+    digits = string.digits
+    return f"{random.choice(letters)}{random.randint(100, 999)}{random.choice(letters)}{random.choice(letters)}"
+
+
+def generate_car_models(session):
+    colors = ['Red', 'Blue', 'Green', 'Black', 'White']
+
+    models = session.query(ModelModel).all()
+
+    for _ in range(10):
+        if models:
+            model = random.choice(models)
+            vin_num = generate_vin()
+            license_plate = generate_license_plate()
+            color = random.choice(colors)
+            car_model = CarModel(vin_num=vin_num, license_plate=license_plate, model_id=model.id, colour=color)
+            session.add(car_model)
+
+    session.commit()
+
+
+# СТАТУСЫ
+
+brand_names_st = ["Pending", "Successful", "Declined", "Cancelled", "Refunded"]
+
+
+def generate_statuses(session):
+    for st in brand_names_st:
+        status = StatusModel(
+            status=st
+        )
+        session.add(status)
+    session.commit()
+
+
+# МЕТОДЫ
+
+methods = ["Credit Card", "Bank Transfer", "Cash", "E-wallet", "Cryptocurrency", "Gift Card"]
+
+
+def generate_methods(session):
+    for mth in methods:
+        method = MethodModel(
+            method=mth
+        )
+        session.add(method)
+    session.commit()
+
+
+# КЛИЕНТЫ
+
+names = [
+    "Ivan", "Petr", "Svetlana", "Olga", "Dmitry", "Sergey",
+    "Anna", "Maria", "Alexey", "Nikita", "Yulia", "Vladimir",
+    "Ekaterina", "Andrey", "Tatiana", "Maxim", "Artem",
+    "Elena", "Oleg", "Irina", "Denis", "Natalia", "Victor",
+    "Sofia", "Mikhail", "Daria", "Alexandra", "Stepan",
+    "Ksenia", "Yevgeny", "Anastasia", "Roman"
+]
+
+surnames = [
+    "Ivanov", "Petrov", "Sidorov", "Smirnov", "Kuznetsov",
+    "Popov", "Vasiliev", "Pavlov", "Lebedev", "Morozov",
+    "Novikov", "Solovyov", "Mikhailov", "Fedorov",
+    "Gusakov", "Kolesnikov", "Borisov", "Nikiforov",
+    "Dmitriev", "Vinogradov", "Alekseev", "Semyonov",
+    "Karpov", "Tarasov", "Zaitsev", "Frolov",
+    "Ermolaev", "Zhuravlev", "Belyakov", "Gromov",
+    "Savelyev"
+]
+
+
+def generate_clients(session):
+    for _ in range(10):
+        name = random.choice(names)
+        surname = random.choice(surnames)
+        telephone = '8' + ''.join(random.choices('0123456789', k=10))
+
+        new_client = ClientModel(name=name, surname=surname, telephone=telephone)
+        session.add(new_client)
+
+    session.commit()
+
+
+# ТАРИФЫ
+
+descriptions = ["Basic Hourly Rate", "Daily Rate", "Weekly Rate", "Monthly Rate", "Premium Hourly Rate"]
+
+costs = [300, 1000, 6500, 25000, 500]
+
+
+def generate_tariffs(session):
+    for d, c in zip(descriptions, costs):
+        tariff = TariffModel(
+            description=d,
+            cost=c,
+        )
+        session.add(tariff)
+    session.commit()
+
+
+# НАРУШЕНИЯ
+
+violations = ["Speeding", "Parking violation", "Damage to vehicle",
+              "Driving without license", "Running red light", "Improper lane change",
+              "Failure to yield", "Driving under influence", "Reckless driving", "Unauthorized passenger",
+              "Smoking in vehicle", "Leaving vehicle unattended", "Driving in restricted area",
+              "Failure to report accident", "Using phone while driving", "Exceeding rental time",
+              "Unauthorized fuel type", "Driving with expired license", "Failure to return vehicle",
+              "Driving without seatbelt"]
+
+fines = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000]
+
+
+def generate_violations(session):
+    clients = session.query(ClientModel).all()
+
+    for v in violations:
+        if clients:
+            client_id = random.choice(clients)
+            date = datetime.now() - timedelta(days=random.randint(1, 365))
+            fine = random.choice(fines)
+            violation = ViolationModel(description=v, date=date, fine=fine, client_id=client_id.id)
+            session.add(violation)
+
+    session.commit()
+
+
+# АРЕНДЫ
+
+def random_date(start, end):
+    return start + timedelta(days=random.randint(0, (end - start).days))
+
+
+def generate_rents(session):
+    tariffs = session.query(TariffModel).all()
+    cars = session.query(CarModel).all()
+    clients = session.query(ClientModel).all()
+
+    if not tariffs or not cars or not clients:
+        print("Нет доступных тарифов, автомобилей или клиентов для генерации.")
+        return
+
+    for _ in range(10):
+        start_date = random_date(datetime.now() - timedelta(days=5 * 365), datetime.now())
+        finish_date = random_date(start_date, start_date + timedelta(
+            days=random.randint(1, 600)))
+
+        tariff_id = random.choice(tariffs).id
+        car_id = random.choice(cars).id
+        client_id = random.choice(clients).id
+
+        new_rent = RentModel(
+            start_date=start_date,
+            finish_date=finish_date,
+            tariff=tariff_id,
+            car_id=car_id,
+            client_id=client_id
+        )
+
+        session.add(new_rent)
+
+    session.commit()
+
+
+# ОПЛАТЫ
+
+def generate_payments(session):
+    methods = session.query(MethodModel).all()
+    statuses = session.query(StatusModel).all()
+    rents = session.query(RentModel).all()
+
+    if not methods or not statuses or not rents:
+        print("Нет доступных методов оплаты, статусов оплаты или аренд для генерации.")
+        return
+
+    for _ in range(10):
+        date = random_date(datetime.now() - timedelta(days=3 * 365), datetime.now())
+
+        rent_id = random.choice(rents).id
+        method_id = random.choice(methods).id
+        status_id = random.choice(statuses).id
+
+        new_payment = PaymentModel(
+            date=date,
+            rent_id=rent_id,
+            method_id=method_id,
+            status_id=status_id
+        )
+
+        session.add(new_payment)
+
+    session.commit()
+
+
+# АРЕНДЫ-ОПЛАТЫ
+
+def generate_rent_violations(session):
+    results = session.query(RentModel, ViolationModel).join(ViolationModel,
+                                                            RentModel.client_id == ViolationModel.client_id).all()
+
+    if not results:
+        print("Нет доступных записей о аренде или нарушениях для генерации.")
+        return
+
+    for rent, violation in results:
+        new_rent_violation = RentViolationModel(
+            rent_id=rent.id,
+            violation_id=violation.id
+        )
+
+        session.add(new_rent_violation)
+
+    session.commit()
+
+
+# ГЕНЕРАЦИЯ
+
+def generation(session):
+    # ветка машин
+    generate_brands(session)
+    generate_models(session)
+    generate_car_models(session)
+    # ветка оплаты
+    generate_statuses(session)
+    generate_methods(session)
+    # ветка клиент-нарушения-аренда
+    generate_clients(session)
+    generate_tariffs(session)
+    generate_violations(session)
+    generate_rents(session)
+    generate_payments(session)
+    # многое-ко_многому
+    generate_rent_violations(session)
---
+++ carBD/main.py
@@ -0,0 +1,309 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import Session, as_declarative, relationship, scoped_session, sessionmaker
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from datetime import datetime
+from markupsafe import escape
+
+engine = create_engine("sqlite:///carDB.db", echo=False)
+app = Flask(__name__)
+
+db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+
+
+@as_declarative()
+class AbstractModel:
+    abstract = True
+
+
+class BrandModel(AbstractModel):
+    __tablename__ = 'brands'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    brand_name = Column(String(30))
+    country = Column(String(30))
+
+
+class ModelModel(AbstractModel):
+    __tablename__ = 'models'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    brand_id = Column(Integer, ForeignKey('brands.id', ondelete='SET NULL'))
+    model_name = Column(String(30))
+
+
+class CarModel(AbstractModel):
+    __tablename__ = 'cars'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    vin_num = Column(String, nullable=False)
+    license_plate = Column(String, nullable=False)
+    model_id = Column(Integer, ForeignKey('models.id', ondelete='SET NULL'))
+    colour = Column(String(20))
+
+
+class ClientModel(AbstractModel):
+    __tablename__ = 'clients'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String, nullable=False)
+    surname = Column(String, nullable=False)
+    telephone = Column(String, nullable=False)
+
+
+class ViolationModel(AbstractModel):
+    __tablename__ = 'violations'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    description = Column(String, nullable=False)
+    date = Column(DateTime)
+    fine = Column(Integer)
+    client_id = Column(Integer, ForeignKey('clients.id', ondelete='SET NULL'))
+
+    rents = relationship('RentViolationModel', back_populates='violation', cascade='all, delete-orphan')
+
+
+class TariffModel(AbstractModel):
+    __tablename__ = 'tariffs'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    description = Column(String, nullable=False)
+    cost = Column(Integer, nullable=False)
+
+
+class RentModel(AbstractModel):
+    __tablename__ = 'rents'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    start_date = Column(DateTime)
+    finish_date = Column(DateTime)
+    tariff = Column(Integer, ForeignKey('tariffs.id', ondelete='SET NULL'))
+    car_id = Column(Integer, ForeignKey('cars.id', ondelete='SET NULL'))
+    client_id = Column(Integer, ForeignKey('clients.id', ondelete='SET NULL'))
+
+    violations = relationship('RentViolationModel', back_populates='rent', cascade='all, delete-orphan')
+
+
+class StatusModel(AbstractModel):
+    __tablename__ = 'statuses'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    status = Column(String, nullable=False)
+
+
+class MethodModel(AbstractModel):
+    __tablename__ = 'paying_methods'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    method = Column(String, nullable=False)
+
+
+class PaymentModel(AbstractModel):
+    __tablename__ = 'payments'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    date = Column(DateTime)
+    rent_id = Column(Integer, ForeignKey('rents.id', ondelete='SET NULL'))
+    method_id = Column(Integer, ForeignKey('paying_methods.id', ondelete='SET NULL'))
+    status_id = Column(Integer, ForeignKey('statuses.id', ondelete='SET NULL'))
+
+
+class RentViolationModel(AbstractModel):
+    __tablename__ = 'rent_violation'
+    rent_id = Column(Integer, ForeignKey('rents.id', ondelete='CASCADE'), primary_key=True)
+    violation_id = Column(Integer, ForeignKey('violations.id', ondelete='CASCADE'), primary_key=True)
+
+    rent = relationship('RentModel', back_populates='violations')
+    violation = relationship('ViolationModel', back_populates='rents')
+
+
+AbstractModel.metadata.drop_all(engine)
+AbstractModel.metadata.create_all(engine)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/brands')
+def brands():
+    session = db_session()
+    brands = session.query(BrandModel).all()
+    session.close()
+    brands_list = [{'id': brand.id, 'brand_name': brand.brand_name, 'country': brand.country} for brand in brands]
+    return jsonify(brands_list)
+
+
+@app.route('/statuses')
+def statuses():
+    session = db_session()
+    statuses = session.query(StatusModel).all()
+    session.close()
+    statuses_list = [{'id': status.id, 'status': status.status} for status in statuses]
+    return jsonify(statuses_list)
+
+
+@app.route('/clients')
+def clients():
+    session = db_session()
+    clients = session.query(ClientModel).all()
+    session.close()
+    clients_list = [{'id': client.id, 'name': client.name, 'surname': client.surname, 'telephone': client.telephone} for
+                    client in clients]
+    return jsonify(clients_list)
+
+
+@app.route('/add_client', methods=['POST'])
+def add_client():
+    name = escape(request.json['name'])
+    surname = escape(request.json['surname'])
+    telephone = escape(request.json['telephone'])
+
+    session = db_session()
+    new_client = ClientModel(name=name, surname=surname, telephone=telephone)
+    session.add(new_client)
+    session.commit()
+    session.close()
+
+    return jsonify({'message': 'Client added successfully'})
+
+
+@app.route('/edit_client/<int:client_id>', methods=['PUT'])
+def edit_client(client_id):
+    name = escape(request.json['name'])
+    surname = escape(request.json['surname'])
+    telephone = escape(request.json['telephone'])
+
+    session = db_session()
+    client = session.query(ClientModel).get(client_id)
+    client.name = name
+    client.surname = surname
+    client.telephone = telephone
+    session.commit()
+    session.close()
+
+    return jsonify({'message': 'Client edited successfully'})
+
+
+@app.route('/delete_client/<int:client_id>', methods=['DELETE'])
+def delete_client(client_id):
+    session = db_session()
+    client = session.query(ClientModel).get(client_id)
+    session.delete(client)
+    session.commit()
+    session.close()
+
+    return jsonify({'message': 'Client deleted successfully'})
+
+
+@app.route('/tariffs')
+def tariffs():
+    session = db_session()
+    tariffs = session.query(TariffModel).all()
+    session.close()
+    tariffs_list = [{'id': tariff.id, 'description': tariff.description, 'cost': tariff.cost} for tariff in tariffs]
+    return jsonify(tariffs_list)
+
+
+@app.route('/cars')
+def cars():
+    session = db_session()
+    cars = session.query(CarModel).all()
+    session.close()
+    cars_list = [{'id': car.id, 'vin_num': car.vin_num, 'license_plate': car.license_plate, 'model_id': car.model_id,
+                  'colour': car.colour} for car in cars]
+    return jsonify(cars_list)
+
+
+@app.route('/payments')
+def payments():
+    session = db_session()
+    payments = session.query(PaymentModel).all()
+    session.close()
+    payments_list = [{'id': payment.id, 'date': payment.date, 'rent_id': payment.rent_id,
+                      'method_id': payment.method_id, 'status_id': payment.status_id} for payment in payments]
+    return jsonify(payments_list)
+
+
+@app.route('/payments')
+def payments():
+    session = db_session()
+    payments = session.query(PaymentModel).all()
+    session.close()
+    payments_list = [{'id': payment.id, 'date': payment.date, 'rent_id': payment.rent_id,
+                      'method_id': payment.method_id, 'status_id': payment.status_id} for payment in payments]
+    return jsonify(payments_list)
+
+@app.route('/rents')
+def rents():
+    session = db_session()
+    rents = session.query(RentModel).all()
+    session.close()
+    rents_list = [{'id': rent.id, 'start_date': rent.start_date, 'finish_date': rent.finish_date, 'tariff': rent.tariff,
+                   'car_id': rent.car_id, 'client_id': rent.client_id} for rent in rents]
+
+    return jsonify(rents_list)
+
+
+@app.route('/add_rent', methods=['POST'])
+def add_rent():
+    start_date_str = escape(request.json['start_date'])
+    finish_date_str = escape(request.json['finish_date'])
+    tariff = escape(request.json['tariff'])
+    car_id = escape(request.json['car_id'])
+    client_id = escape(request.json['client_id'])
+
+    start_date = datetime.strptime(start_date_str, '%Y-%m-%dT%H:%M')
+    finish_date = datetime.strptime(finish_date_str, '%Y-%m-%dT%H:%M')
+
+    session = db_session()
+    new_rent = RentModel(
+        start_date=start_date,
+        finish_date=finish_date,
+        tariff=tariff,
+        car_id=car_id,
+        client_id=client_id
+    )
+    session.add(new_rent)
+    session.commit()
+    session.close()
+
+    return jsonify({'message': 'Rent added successfully'})
+
+
+@app.route('/edit_rent/<int:rent_id>', methods=['PUT'])
+def edit_rent(rent_id):
+    start_date_str = escape(request.json['start_date'])
+    finish_date_str = escape(request.json['finish_date'])
+    tariff = escape(request.json['tariff'])
+    car_id = escape(request.json['car_id'])
+    client_id = escape(request.json['client_id'])
+
+    start_date = datetime.strptime(start_date_str, '%Y-%m-%dT%H:%M')
+    finish_date = datetime.strptime(finish_date_str, '%Y-%m-%dT%H:%M')
+
+    session = db_session()
+    rent = session.query(RentModel).get(rent_id)
+    rent.start_date = start_date
+    rent.finish_date = finish_date
+    rent.tariff = tariff
+    rent.car_id = car_id
+    rent.client_id = client_id
+    session.commit()
+    session.close()
+
+    return jsonify({'message': 'Rent edited successfully'})
+
+
+@app.route('/delete_rent/<int:rent_id>', methods=['DELETE'])
+def delete_rent(rent_id):
+    session = db_session()
+    rent = session.query(RentModel).get(rent_id)
+    session.delete(rent)
+    session.commit()
+    session.close()
+
+    return jsonify({'message': 'Rent deleted successfully'})
+
+
+@app.route('/test')
+def test():
+    return {"result": "success"}
+
+
+from generation import generation
+
+if __name__ == "__main__":
+    with Session(bind=engine) as session:
+        generation(session)
+        app.run(debug=True)
